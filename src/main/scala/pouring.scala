a7ace729bc08d3245883eb7e0e79121e37e1a54f
Martin Nigsch <martin@nigsch.com>
2019-04-11 16:43:54 +0200

    next

--- src/main/scala/pouring.scala
+++ src/main/scala/pouring.scala
@@ -1,4 +1,5 @@
 // from Coursera course
+package pouring
 
 class Pouring(capacity: Vector[Int]) {
 
@@ -9,10 +10,22 @@ class Pouring(capacity: Vector[Int]) {
 
   // Moves
 
-  trait Move
-  case class Empty(glass: Int) extends Move
-  case class Fill(glass: Int) extends Move
-  case class Pour(from: Int, to: Int) extends Move
+  trait Move {
+    def change(state: State): State
+  }
+
+  case class Empty(glass: Int) extends Move {
+    def change(state: State) = state updated (glass, 0)
+  }
+  case class Fill(glass: Int) extends Move {
+    def change(state: State) = state updated (glass, capacity(glass))
+  }
+  case class Pour(from: Int, to: Int) extends Move {
+    def change(state: State) =  {
+      val amount = state(from) min (capacity(to) - state(to))
+      state updated (from, state(from) - amount) updated (to, state(to))
+    }
+  }
 
   val glasses = 0 until capacity.length
 
@@ -21,4 +34,36 @@ class Pouring(capacity: Vector[Int]) {
       (for (g <- glasses) yield Fill(g)) ++
       (for (from <- glasses; to <- glasses if from != to ) yield Pour(from, to))
 
+  // Paths
+
+  class Path(history: List[Move]) {
+    def endState: State = (history foldRight initialState) (_ change _)
+
+    def extend(move: Move) = new Path(move :: history)
+
+    override def toString = (history.reverse mkString " ") + "-->" + endState
+
+    // pattern matching solution
+    /*
+    def endState: State = trackState(history)
+    private def trackState(xs: List[Move]): State = xs match {
+      case Nil => initialState
+      case move :: xs1 => move change trackState(xs1)
+    }
+    */
+  }
+
+  val initialPath = new Path(Nil)
+
+  def from(paths: Set[Path]): Stream[Set[Path]] =
+    if (paths.isEmpty) Stream.empty
+    else {
+      val more = for {
+        path <- paths
+        next <- moves map path.extend
+      } yield next
+      paths #:: from(more)
+    }
+
+  val pathSets = from(Set(initialPath))
 }
